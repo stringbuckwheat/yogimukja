38b2de8731d1f4debcf2e8691de1187333b5483a
stringbuckwheat <stringbuckwheat@gmail.com>
2024-09-02 10:45:13 +0900

    [Chore] Initial commit

---
+++ src/main/java/com/memil/yogimukja/YogimukjaApplication.java
@@ -0,0 +1,13 @@
+package com.memil.yogimukja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogimukjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogimukjaApplication.class, args);
+	}
+
+}
